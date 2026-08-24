1edb5683fbc7476403bee75a286b5c0c657febba
hachipaws128 <hachipaws128@gmail.com>
2026-08-24 11:24:51 +0800

    Activity 4

---
+++ activity4.py
@@ -0,0 +1,4 @@
+sentence = "I'm deeply inlove to the person that wasn't even love me, \n\tWe met but not at the right time. \n\t\tWe saw each other but that feelings we had are completely gone, \n\t\t\tWe're both know that we had that feelings and yet we hide it to protect ourselves to this love was it called."
+
+
+print(sentence)

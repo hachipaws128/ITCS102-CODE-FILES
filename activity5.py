b75168f37a5a7a8a3d861751f27078ea7a5b34da
hachipaws128 <hachipaws128@gmail.com>
2026-08-26 11:24:53 +0800

    BSIT 1B ACTIVITY 5

---
+++ activity5.py
@@ -0,0 +1,12 @@
+#operators activity
+
+a = 24
+b = 19
+sum = a + b
+diff = a-b
+product = a*b
+qoutient = a / b
+
+print("The sum of",a,"and", b,"is",sum)
+print("The difference of",a,"minus", b,"is",diff)
+print("The product",a, "times", b,"is",product)

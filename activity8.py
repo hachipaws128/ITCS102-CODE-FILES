c7f2ca10413e524d406f75522bee7d1020d532ee
hachipaws128 <hachipaws128@gmail.com>
2026-09-09 20:41:22 +0800

    Assignment act 8

---
+++ activity8.py
@@ -0,0 +1,35 @@
+# STRING ASSIGNMENT OPERATOR
+
+loves = ""
+
+a1 = input("What are your hobbies? (One at a time) ")
+loves += a1 + ", "
+
+a2 = input("What else? ")
+loves += a2 + ", "
+
+a3 = input("What else? ")
+loves += a3 + ", "
+
+a4 = input("What else? ")
+loves += a4 + ", "
+
+a5 = input("What else? ")
+loves += a5 + ", "
+
+a6 = input("What else? ")
+loves += a6 + ", "
+
+a7 = input("What else? ")
+loves += a7 + ", "
+
+a8 = input("What else? ")
+loves += a8 + ", "
+
+a9 = input("What else? ")
+loves += a9 + ", "
+
+a0 = input("What else? ")
+loves += a0 + "."
+
+print("Your loves are: {", loves, "}")

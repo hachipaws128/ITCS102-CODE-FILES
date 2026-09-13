79954439f0a8b5eb4587513666454aa78a3d1295
hachipaws128 <hachipaws128@gmail.com>
2026-09-13 22:10:32 +0800

    Code challenge 3

--- Code_Challenge_3.py
+++ Code_Challenge_3.py
@@ -1,41 +1,36 @@
 #Global Freight Calculator
 
-
-
+## This program calculates the shipping cost based on user input for item type, weight, distance, and shipping options.
 name = input("Put your name -->> ")
 product = input("Put your product name -->> ")
 weight = float(input("Put your weight (kg) of product -->> "))
 distance = float(input("How far is your Location (km)? -->> "))
 is_express = input("Is it Express? (True/False) -->> ").upper() == "true"
 is_international = input("Is it International? (True/False) -->> ").upper() == "true"
-is_rush = input("Do you want to rush your order? (True/False) -->> ").upper() == "true"
 fragile = input("Is it Fragile? (True/False) -->> ").upper() == "true"
 
+#Calculating the base cost of shipping
 base_cost = (weight * 2.50) + (distance * 0.15)
 
-if weight <= 2.0 and distance <= 50 and is_express == True and is_international == True and is_rush == True:
+# Calculating the total cost of shipping based on conditions
+if weight <= 2.0 and distance <= 100 and is_express == True and is_international == False:
     total = 0.00
-
-elif is_express:
-    total = base_cost * 1.5
-
-elif is_international:
-    total = base_cost * 2.0
-
-elif is_rush:
-    total = base_cost * 1.70
-
+elif is_international and is_express:
+    total = (base_cost * 1.40) + 50
+elif is_express or (is_international and weight > 20):
+    total = (base_cost * 1.20) + 25
+elif weight > 10 or distance > 1000:
+    total = base_cost + 30
 else:
     total = base_cost
 
+# Displaying the shipping details
 print("\n============...Shipping Details...============")
 print("Client Name: ", name)
 print("Type of Item: ", product)
 print("Shipping Cost: $", total)
-print("=============...Kg & Km Details...============")
 print("Weight: ", weight, "kg")
 print("Distance: ", distance, "km")
-print("=============...Shipping Options...============")
 print("Is international: ", is_international)
 print("Is express: ", is_express)
 print("Is fragile: ", fragile)

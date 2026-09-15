2cb6fd3688bf23fee98cb46d32d9fbaa3b7d24a1
hachipaws128 <hachipaws128@gmail.com>
2026-09-15 19:13:22 +0800

    Refactor loan eligibility checks and interest rates

--- activity13.py
+++ activity13.py
@@ -9,25 +9,35 @@ has_collateral = bool(input("Do you have any collateral? -->> "))
 base_interest = 0.0
 
 #baseline eligibility
-if age >= 21 and is_employed == True:
+if (age >= 21 and age <= 75) and is_employed == True:
     if credit_score >= 750:
-        if has_collateral == True:
-            pass
-        elif True:
-            pass
+        if annual_income >= 100000:
+            base_interest = 4.5
         else:
-            pass
+            base_interest = 5.0
+    elif 600 <= credit_score < 750:
+        if has_collateral == True:
+            base_interest = 7.0
+        elif annual_income < 40000:
+            base_interest = 9.5
+        elif has_collateral == True and annual_income < 40000:
+            base_interest = 8.0
+        else:
+            base_interest = 8.0
     else:
-        pass
+        print("============...Rejected...============ \nRejected: Credit score too low. \nRequirements: Credit score must be at least 600. \n\nPlease try again. \n=======================================")
+        exit()
 else:
-    print("Declined, You did not reach the requirements")
+    print("============...Rejected...============ \nRejected: Failed to meet the requirements. \nRequirements: Age between 21 and 75, and must be employed. \n\nPlease try again. \n=======================================")
+    exit()
 
-if age >= 21 and is_employed == True:
-    if credit_score >= 750:
-        if has_collateral == False:
+print("\nYou have been approved for a loan with the following details: ")
 
+print ("\n============...Details...============")
+print("Age:", age)
+print("Is employed:", is_employed)
+print("Credit Score:", credit_score)
+print("Annual Income:", annual_income)
+print("Has Collateral:", has_collateral)
 
-##f credit_score >=
-
-
-# 500 below is low-credit score 750 is considered as high-credit score
+print("=======================================")
